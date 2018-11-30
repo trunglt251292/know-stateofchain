13b9b242f8b109421f6e6012bd42707f38606f6a
Nguyen Thanh Nhan <nhannguyen@vinaecom.com>
2018-12-01 00:15:13 +0700

    Init UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import search from './baseline-search-24px.svg';
 import './App.css';
+import { Button, InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="Header-btns">
+            <Button outline color="secondary" size="sm" className="Header-btn">Submit</Button>
+            <Button color="primary" size="sm" className="Header-btn">Registry</Button>
+          </div>
+          <img src="https://knownetwork.io/images/logo-white2x.png" className="App-logo" alt="logo" />
+          <InputGroup className="Search-bar">
+            <Input />
+            <InputGroupAddon addonType="append" className="Search-icon">
+              <InputGroupText>
+                <img src={search} alt="Search icon"/>
+              </InputGroupText>
+            </InputGroupAddon>
+          </InputGroup>
+          <span className="App-link">
+            Distributed Certifications and Degrees
+          </span>
         </header>
       </div>
     );
