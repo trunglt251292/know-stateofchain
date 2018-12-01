047e30d917dd189ffd0f4c0fcd182da65227721b
Nguyen Thanh Nhan <nhannguyen@vinaecom.com>
2018-12-01 09:51:10 +0700

    Add registry form

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import search from './baseline-search-24px.svg';
 import { Button, InputGroup, InputGroupAddon, InputGroupText, Input,
-  Modal, ModalHeader, ModalBody, ModalFooter
+  Modal, ModalHeader, ModalBody, ModalFooter,
+  Form, FormGroup, Label,
 } from 'reactstrap';
 
 class HomePage extends Component {
@@ -12,14 +13,26 @@ class HomePage extends Component {
     this.handleSearch = this.handleSearch.bind(this);
     this.handleSearchChange = this.handleSearchChange.bind(this);
     this.toggleModalSubmit = this.toggleModalSubmit.bind(this);
+    this.toggleModalRegistry = this.toggleModalRegistry.bind(this);
     this.state = {
       searchContent: '',
       modalSubmit: false,
+      modalRegistry: false,
+      organizationName: '',
+      organizationCode: '',
     };
   }
 
   toggleModalSubmit() {
     this.setState(prevState => ({ modalSubmit: ! prevState.modalSubmit }));
+  }
+
+  toggleModalRegistry() {
+    this.setState(prevState => ({
+      modalRegistry: ! prevState.modalRegistry,
+      organizationName: '',
+      organizationCode: '',
+    }));
   }
 
   handleSubmit() {
@@ -28,7 +41,8 @@ class HomePage extends Component {
   };
 
   handleRegistry() {
-    console.log('handleRegistry');
+    console.log('handleRegistry', this.state);
+    this.toggleModalRegistry();
   };
 
   handleSearch() {
@@ -45,6 +59,51 @@ class HomePage extends Component {
       this.handleSearch();
     }
   };
+
+  handleOrganizationNameChange = (e) => {
+    this.setState({ organizationName: e.target.value });
+  };
+
+  handleOrganizationCodeChange = (e) => {
+    this.setState({ organizationCode: e.target.value });
+  };
+
+  renderModalRegistry() {
+    return (
+      <Modal isOpen={this.state.modalRegistry} toggle={this.toggleModalRegistry}>
+        <ModalHeader toggle={this.toggleModalRegistry}>Registry your organization</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label for="organizationName">Organization Name</Label>
+              <Input onChange={this.handleOrganizationNameChange} type="text" name="organizationName" id="organizationName" placeholder="Your organization?" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="organizationCode">Organization Code</Label>
+              <Input onChange={this.handleOrganizationCodeChange} type="text" name="organizationCode" id="organizationCode" placeholder="Your code?" />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="secondary" onClick={this.toggleModalRegistry}>Cancel</Button>{' '}
+          <Button color="primary" onClick={this.handleRegistry}>Submit</Button>
+        </ModalFooter>
+      </Modal>
+    );
+  }
+
+  renderModalSubmit() {
+    return <Modal isOpen={this.state.modalSubmit} toggle={this.toggleModalSubmit}>
+      <ModalHeader toggle={this.toggleModalSubmit}>Modal title</ModalHeader>
+      <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={this.handleSubmit}>Submit</Button>{' '}
+        <Button color="secondary" onClick={this.toggleModalSubmit}>Cancel</Button>
+      </ModalFooter>
+    </Modal>;
+  }
 
   render() {
     return (
@@ -65,16 +124,12 @@ class HomePage extends Component {
         <span className="App-link">
           Distributed Certifications and Degrees
         </span>
-        <Modal isOpen={this.state.modalSubmit} toggle={this.toggleModalSubmit}>
-          <ModalHeader toggle={this.toggleModalSubmit}>Modal title</ModalHeader>
-          <ModalBody>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </ModalBody>
-          <ModalFooter>
-            <Button color="primary" onClick={this.handleSubmit}>Submit</Button>{' '}
-            <Button color="secondary" onClick={this.toggleModalSubmit}>Cancel</Button>
-          </ModalFooter>
-        </Modal>
+        {
+          this.renderModalSubmit()
+        }
+        {
+          this.renderModalRegistry()
+        }
       </header>
     );
   }
